c04395a3002b7794c2ea03c61a4434f6c7ecc976
Joe Maller <joe@joemaller.com>
2025-03-04 14:15:39 -0700

    format-wp-json updates, ue detect-indent

--- test/format-wp-json.test.js
+++ test/format-wp-json.test.js
@@ -2,70 +2,64 @@ import { expect, test } from "vitest";
 import { formatWPJson } from "../lib/format-wp-json.js";
 
 import { readFile } from "node:fs/promises";
+import detectIndent from "detect-indent";
 
 const fixturesPath = "test/fixtures/format";
 
 test("Format WP JSON collapses color arrays", async () => {
-  const src = JSON.parse(
-    (await readFile(`${fixturesPath}/palette-theme.json`)).toString()
-  );
+  const src = (await readFile(`${fixturesPath}/palette-theme.json`)).toString();
+
+  const input = JSON.parse(src);
   const expected = (
     await readFile(`${fixturesPath}/palette-theme-formatted.json`)
   ).toString();
 
-  const actual = await formatWPJson(src);
+  const indent = detectIndent(src);
+  const actual = await formatWPJson(input, indent);
 
   expect(actual).toEqual(expected);
 });
 
-test("Format WP JSON 0 spaces", async () => {
-  const src = JSON.parse(
-    (await readFile(`${fixturesPath}/palette-theme.json`)).toString()
-  );
-  const expected = (
-    await readFile(`${fixturesPath}/palette-theme-formatted-0spaces.json`)
-  ).toString();
+// test("Format WP JSON 0 spaces", async () => {
+//   const src = (await readFile(`${fixturesPath}/palette-theme.json`)).toString();
+//   const input = JSON.parse(src);
 
-  const actual = await formatWPJson(src, 0);
+//   const expected = (
+//     await readFile(`${fixturesPath}/palette-theme-formatted-0spaces.json`)
+//   ).toString();
 
-  expect(actual).toEqual(expected);
-});
+//   const actual = await formatWPJson(input, 0);
 
-test("Format WP JSON 2 spaces", async () => {
-  const src = JSON.parse(
-    (await readFile(`${fixturesPath}/palette-theme.json`)).toString()
-  );
-  const expected = (
-    await readFile(`${fixturesPath}/palette-theme-formatted-2spaces.json`)
-  ).toString();
-
-  const actual = await formatWPJson(src, 2);
-
-  expect(actual).toEqual(expected);
-});
-
-test("Format WP JSON 4 spaces", async () => {
-  const src = JSON.parse(
-    (await readFile(`${fixturesPath}/palette-theme.json`)).toString()
-  );
-  const expected = (
-    await readFile(`${fixturesPath}/palette-theme-formatted-4spaces.json`)
-  ).toString();
-
-  const actual = await formatWPJson(src, 4);
-
-  expect(actual).toEqual(expected);
-});
+//   expect(actual).toEqual(expected);
+// });
 
 test("Condense font-sizes", async () => {
-  const src = JSON.parse(
-    (await readFile(`${fixturesPath}/font-sizes-theme.json`)).toString()
-  );
+  const src = (
+    await readFile(`${fixturesPath}/font-sizes-theme.json`)
+  ).toString();
+  const input = JSON.parse(src);
   const expected = (
     await readFile(`${fixturesPath}/font-sizes-theme-formatted.json`)
   ).toString();
 
-  const actual = await formatWPJson(src);
+  const indent = detectIndent(src);
+  const actual = await formatWPJson(input, indent);
 
   expect(actual).toEqual(expected);
 });
+
+test("Condense margin and padding", async () => {
+  const src = (
+    await readFile(`${fixturesPath}/block-variation.json`)
+  ).toString();
+  const input = JSON.parse(src);
+  const expected = (
+    await readFile(`${fixturesPath}/block-variation-formatted.json`)
+  ).toString();
+
+  const indent = detectIndent(src);
+  const actual = await formatWPJson(input, indent);
+
+  expect(actual).toEqual(expected);
+});
+
