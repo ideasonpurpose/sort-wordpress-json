7e875785d849e1ba6c5b9e489a41901d2ae079b4
Joe Maller <joe@joemaller.com>
2025-04-10 14:59:02 -0400

    work on end-to-end equality tests

--- test/end-to-end-integrity.test.js
+++ test/end-to-end-integrity.test.js
@@ -1,15 +1,21 @@
-import { expect, test } from "vitest";
-import { schemaSort, resolveProps } from "../index.js";
+import { expect, test, describe } from "vitest";
+import { schemaSort } from "../lib/schema-sort.js";
+import { getSchema } from "../lib/get-schema.js";
 
-import $RefParser from "@apidevtools/json-schema-ref-parser";
+import { readdir, readFile } from "node:fs/promises";
 
-import { readdir, readFile, writeFile } from "node:fs/promises";
-
-test.skip("Sorted JSON is equal to original JSON", async () => {
-  const JSONfiles = (await readdir("./test/fixtures/sort")).filter(
+describe.skip("Sorted JSON equals original JSON", async () => {
+  const jsonFiles = (await readdir("./test/fixtures/sort")).filter(
     (f) => f.endsWith(".json") && !f.endsWith("-sorted.json")
   );
 
-  // console.log(JSONfiles);
-  // TODO: Compare input json file to processed output json file for deep equivalence
+  test.each(jsonFiles)("jsonFile: %s", async (jsonFile) => {
+    const expected = JSON.parse(
+      await readFile(`./test/fixtures/sort/${jsonFile}`)
+    );
+    const schema = await getSchema(expected);
+
+    const actual = await schemaSort(expected, schema);
+    expect(actual).toEqual(expected);
+  });
 });
